33d282d16b843b059bcedbd43ffaf0ec677ef941
sg495 <sg495@users.noreply.github.com>
2022-01-16 17:05:36 +0000

    Update __init__.py
    Version bump following bugfix.

--- dag_cbor/__init__.py
+++ dag_cbor/__init__.py
@@ -2,7 +2,7 @@
     Python implementation of the `DAG-CBOR codec <https://ipld.io/specs/codecs/dag-cbor/spec/>`_ specification.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .encoding import encode
 from .decoding import decode
